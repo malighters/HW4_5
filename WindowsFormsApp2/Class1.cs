67018da28c26c7518f2b044b3d349eb83b20a0ab
USER <USER@USER-PC>
2022-10-04 19:50:27 +0300

    Add project files.

---
+++ WindowsFormsApp2/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    public class Stuff
+    {
+        public int Price { get; set; }
+        public string Country { get; set; }
+        public string Name { get; set; }
+        public string ProductionDate { get; set; }
+        public string Description { get; set; }
+
+        public Stuff(int price, string country, string name, string productionDate, string description)
+        {
+            Price = price;
+            Country = country;
+            Name = name;
+            ProductionDate = productionDate;
+            Description = description;
+        }
+    }
+
+    public class Goods : Stuff
+    {
+        private readonly string _expiryDate;
+        private readonly double _quantity;
+        private readonly string _unit;
+
+        public Goods(int price, string country, string name, string productionDate,
+            string description, string expiryDate, double quantity, string unit) : base(price, country, name, productionDate, description)
+        {
+            _expiryDate = expiryDate;
+            _quantity = quantity;
+            _unit = unit;
+        }
+    }
+
+    public class Book : Stuff
+    {
+        private readonly int _pageNumber;
+        private readonly string _publisher;
+        private readonly string _author;
+
+        public Book(int price, string country, string name, string productionDate, string description,
+            int pageNumber, string publisher, string author) : base(price, country, name, productionDate, description)
+        {
+            _pageNumber = pageNumber;
+            _publisher = publisher;
+            _author = author;
+        }
+
+
+    }
+}
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+
+        DataTable table1 = new DataTable();
+        List<Stuff> list = new List<Stuff>();
+        public List<Stuff> Stuffs { get; set; }
+        public Form1()
+        {
+            Stuffs = GetStuffs();
+            InitializeComponent();
+        }
+
+        private List<Stuff> GetStuffs()
+        {
+            
+            
+            return list;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+       
+            table1.Columns.Add("Name", typeof(string));
+            table1.Columns.Add("Price", typeof(int));
+            table1.Columns.Add("Country", typeof(string));
+            table1.Columns.Add("Packing date", typeof(string));
+            table1.Columns.Add("Description", typeof(string));
+
+            Goods tang = new Goods(105, "Spain", "Tangarines", "25.09.2022", "Fruit", "25.10.2022", 200, "kg");
+            list.Add(tang);
+            Book book = new Book(155, "Great Britain", "1984", "23.05.2021", "Book", 215, "Bloomberg", "George Orwell");
+            list.Add(book);
+
+            dataGridView1.DataSource = table1;
+
+
+            for (int i = 0; i < Stuffs.Count; i++)
+            {
+                table1.Rows.Add(Stuffs[i].Name, Stuffs[i].Price, Stuffs[i].Country, Stuffs[i].ProductionDate, Stuffs[i].Description);
+            }
+
+
+
+        }
+
+      
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.RemoveAt(dataGridView1.Rows.Count - 2);
+
+        }
+    }
+}
